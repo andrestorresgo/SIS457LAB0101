7c5f327d5b8b27df8d6c49f2bac15cc06c891260
Andrés Torres González <andytorresgonz@gmail.com>
2022-04-03 21:27:27 -0400

    Cambio de estructura de control

--- PSIS457LAB0101/principal.cpp
+++ PSIS457LAB0101/principal.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int flag;
 int main() {
-	cout << "cambiar por biografía" << endl;
+	do {
+		cout << "biografía" << "\n";
+			cout << "Ingrese 1 si desea volver a mostrar la biografía, y cualquier otro caracter para salir" "\n";
+			cin >> flag;
+	} while (flag == 1);
 
 	system("pause");
 	return 0;
